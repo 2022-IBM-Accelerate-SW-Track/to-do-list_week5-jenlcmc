2d9397ed0c950d9b111c211cfb20669c545710a2
Uyen Tran <kiity2509@gmail.com>
2022-07-11 22:56:35 -0700

    finish the lab

--- src/component/SearchTodo.js
+++ src/component/SearchTodo.js
@@ -3,50 +3,80 @@ import { Button, TextField } from "@mui/material";
 import Axios from "axios";
 
 class SearchTodo extends Component {
-  
-  state = {
-    tmpdata: [],
-  };
+	state = {
+		tmpdata: [],
+	};
 
-  handleChange = (e) => {
-    this.setState({
-      content: e.target.value,
-      date: Date().toLocaleString('en-US'),
-    });
-  };
-  
+	handleChange = (e) => {
+		this.setState({
+			content: e.target.value,
+			date: Date().toLocaleString("en-US"),
+		});
+	};
 
-  handleSubmit = (e) => {
-    //Begin Here
-    
-  };
-  
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <TextField
-            id="search-item-input"
-            label="Search for ToDo Item"
-            variant="outlined"
-            onChange={this.handleChange}
-            value={this.state.value}
-          /> 
-          <Button
-            id="search-item-button"
-            name='submit'
-            style={{ marginLeft: "10px",marginTop:10 }}
-            onClick={this.handleSubmit}
-            variant="contained"
-            color="primary"
-          >
-            Search
-          </Button>
-        </form>
-        <div>{this.state.tmpdata}</div>
-      </div>
-    );
-  }
+	// main things they will do is submit a form
+	// which will trigger the call to the searchitem backend service,
+	// as part of that submit we will take the name of the Todo to search for
+	// from the "this.state.content" parameter,
+	// the a user would type in the UI text box.
+
+	//also we have state associated with this component "tmpdata",
+	// this state will be set to the data returned from the backend service
+	// via the "this.setState({tmpdata: JSON.stringify(res.data),});"
+	// code we just put in the HandleSubmit method.
+
+	// will use this state in the render function, underneath the search UI components you'll see "
+	// {this.state.tmpdata}
+	// this is empty initially, because we haven't searched for anything,
+	// but once you supply a search parameter and click the "Search" button,
+	// we will set the state in the HandleSubmit,
+	// which will then update the state in our div to hold the return data from the backend service for the search.
+	handleSubmit = (e) => {
+		//Begin Here
+		e.preventDefault();
+		// HTTP Client to send a GET request
+		Axios({
+			method: "GET",
+			url: "http://localhost:8080/get/searchitem",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			params: {
+				taskname: this.state.content,
+			},
+		}).then((res) => {
+			this.setState({
+				tmpdata: JSON.stringify(res.data),
+			});
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<TextField
+						id="search-item-input"
+						label="Search for ToDo Item"
+						variant="outlined"
+						onChange={this.handleChange}
+						value={this.state.value}
+					/>
+					<Button
+						id="search-item-button"
+						name="submit"
+						style={{ marginLeft: "10px", marginTop: 10 }}
+						onClick={this.handleSubmit}
+						variant="contained"
+						color="primary"
+					>
+						Search
+					</Button>
+				</form>
+				<div>{this.state.tmpdata}</div>
+			</div>
+		);
+	}
 }
 
 export default SearchTodo;
--- src/component/TodoData.js
+++ src/component/TodoData.js
@@ -1,9 +1,33 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
+// makes use of the "useEffect" hook in react, and the await keyword,
+// this combination is essentially telling react to wait for a call to a backend service to complete,
+// then proceeds with the rest of the render. Remember that nodeJS is asynchronous platform,
+// so statements can get executed before data is prepared and ready to return.
+// In the case of our Axios.get above, if we didn't have the await in front of it
+// then the rest of the code will proceed and attempt to render before our response is returned from the backend service.
+// To solve this we said that this function is asynchronous and hence we will receive a response from the backend service before proceeding.
 
+// An alternative is you could do something like we've seen in other services,
+// store the response in state and update it as the data is returned,
+// an example of this will be used in the search functionality next.
 const ShowTodos = () => {
-  //Begin Here
-  
-}
+	//Begin Here
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const res = await Axios.get("http://localhost:8080/get/items");
+				setTodos(JSON.stringify(res.data));
+				console.log(JSON.stringify(res.data));
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		fetchData();
+	}, []);
+	return <div>{todos}</div>;
+};
 export default ShowTodos;
